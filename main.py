e659efbf30d86f07c8ddc57dbde195366716e68c
JoseDRamirezM <eskatejose@hotmail.com>
2020-07-23 10:51:58 -0500

    update

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from pyswip import Prolog
 prolog = Prolog()
 prolog.consult('laberinto.pl')
 
-
 camino = []
 
 def leer(nombre_archivo):
@@ -25,14 +24,14 @@ def buscar(x, y, matriz):
   elif matriz[x][y] == '-':
       #print ('no puedo %d,%d' % (x, y))
       return False
-  elif matriz[x][y] == '!':
+  elif matriz[x][y] == '▄':
       #print ('ya estuve %d,%d' % (x, y))
       return False
   else: camino.append([matriz[x][y]])
 
   #print ('actual %d,%d' % (x, y))
   
-  matriz[x][y] = '!'
+  matriz[x][y] = '▄'
   if ((x < len(matriz)-1 and buscar(x+1, y,matriz)) or (y > 0 and buscar(x, y-1,matriz)) or (x > 0 and buscar(x-1, y,matriz)) or (y < len(matriz[0])-1 and buscar(x, y+1,matriz))):
     return True
   return False
@@ -42,7 +41,6 @@ def init(lista, matriz):
     print("SOLUCIONADO!")
   else : print(":(")
 
-
 def buscar_inicio(matriz,cont):
     if matriz == []:
         return (-1,-1)
@@ -50,22 +48,16 @@ def buscar_inicio(matriz,cont):
         return ([cont,matriz[0].index("i")])
     return buscar_inicio(matriz[1:],cont+1)
 
-def generar_assertz(camino, hechos):
-  for x in range(0 , len(camino) - 1):
-    hechos.append('conecta(%s,%s)' % (camino[x][0], camino[x+1][0]))
-  return hechos
-
 def generar_hechos(camino):
   for x in range(0 , len(camino) - 1):
     prolog.assertz("conecta(%s,%s)" % (camino[x][0], camino[x+1][0]))
-    #print("conecta(%s,%s)" % (camino[x][0], camino[x+1][0]))
-      
+  
 lab = get_laberinto("laberinto-in.txt")
 print('\n'.join([''.join(['{:4}'.format(item) for item in row]) 
-      for row in lab]))
+      for row in lab])) 
 
 init(buscar_inicio(lab,0),lab)
-hechos = []
 generar_hechos(list(filter(lambda a: a != ['*'], camino)))
 sol = list(prolog.query("sol"))
-
+print('\n'.join([''.join(['{:4}'.format(item) for item in row]) 
+      for row in lab])) 
